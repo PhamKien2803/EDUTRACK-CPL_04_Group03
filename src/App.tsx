3ced1316f65b393373337fe0f270397ea035be81
PhuongHE173077 <phuongddhe173077@fpt.edu.vn>
2024-10-28 16:44:55 +0700

    router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { Fragment } from "react/jsx-runtime"
 import DicussionPage from "./page/StudentHome/DicussionPage"
+import { ExamDetail } from "./components/Exam/ExamDetail"
 
 function App() {
 
   return (
     <Fragment>
-      <DicussionPage />
+      <ExamDetail />
     </Fragment>
   )
 }
