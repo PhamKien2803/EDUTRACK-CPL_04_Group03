bc543d0152f3ca452e6bcfd2949c17884a5e69ae
TrungNghiaX <104517120+nnghiax@users.noreply.github.com>
2024-10-28 16:12:51 +0700

    add home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
-import { Fragment } from "react/jsx-runtime"
-// import DicussionPage from "./page/StudentHome/DicussionPage"
-import { ExamDetail } from "./components/Exam/ExamDetail"
+import { Fragment } from "react/jsx-runtime";
+import DicussionPage from "./page/StudentHome/DicussionPage";
+import HomePage from "./page/HomePage/HomePage";
+// import { ExamDetail } from "./components/Exam/ExamDetail"
 
 function App() {
-
   return (
     <Fragment>
+      <HomePage />
       {/* <DicussionPage /> */}
-      <ExamDetail />
+      {/* <ExamDetail /> */}
     </Fragment>
-  )
+  );
 }
 
-export default App
+export default App;
