96dfeaaad527e087c50a26f14241a04b124a1072
PhamKien <kienpdhe170155@fpt.edu.vn>
2024-10-29 10:53:27 +0700

    Set Up Redux
    Set Up Redux

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from "axios";
-import { App } from './App';
+import App from './App';
 axios.defaults.baseURL = "http://localhost:9999";
 
 createRoot(document.getElementById('root')!).render(
