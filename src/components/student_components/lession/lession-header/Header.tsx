995e835b7aa8fbdfa7a40c1c4c2d9951c57710b6
TrungNghiaX <104517120+nnghiax@users.noreply.github.com>
2024-10-31 14:18:47 +0700

    code

--- src/components/student_components/lession/lession-header/Header.tsx
+++ src/components/student_components/lession/lession-header/Header.tsx
@@ -1,72 +1,79 @@
-import { useState } from 'react';
+import { useState } from "react";
 import "../css/Lession.css";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 const Header: React.FC = () => {
-    // State to track the selected option from the dropdown
-    const [activityFilter, setActivityFilter] = useState('All Activities');
-    const [isVisible, setIsVisible] = useState(true);
-    const navigate = useNavigate();
+  // State to track the selected option from the dropdown
+  const [activityFilter, setActivityFilter] = useState("All Activities");
+  const [isVisible, setIsVisible] = useState(true);
+  const navigate = useNavigate();
 
-    const toggleVisibility = () => {
-        setIsVisible(!isVisible);
-    };
-    const handleSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-        setActivityFilter(event.target.value);
-    };
+  const toggleVisibility = () => {
+    setIsVisible(!isVisible);
+  };
+  const handleSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setActivityFilter(event.target.value);
+  };
 
-    const navigateToExam = () => {
-        navigate("/examtest");
-    }
+  const navigateToExam = () => {
+    navigate("/exam-test");
+  };
 
+  return (
+    <div>
+      <div className="course-container">
+        <nav className="breadcrumb">
+          <a href="#">Home</a> /{" "}
+          <a>ENW492c ↔ Academic Writing Skills_Kỹ năng viết học thuật</a>
+        </nav>
+      </div>
+      {isVisible && (
+        <div className="select-container">
+          <select
+            value={activityFilter}
+            onChange={handleSelectChange}
+            className="activity-select"
+          >
+            <option>All Activities</option>
+            <option>Hidden</option>
+            <option>On Going</option>
+            <option>Cancelled</option>
+            <option>Completed</option>
+            <option>Not Started</option>
+            <option>Assignment or Feedback</option>
+          </select>
 
-    return (
-        <div>
+          <select>
+            <option>Slot 1</option>
+            <option>Slot 2</option>
+            <option>Slot 3</option>
+            <option>Slot 4</option>
+            <option>Slot 5</option>
+            <option>Slot 6</option>
+          </select>
 
-            <div className="course-container">
-                <nav className="breadcrumb">
-                    <a href="#">Home</a> / <a>ENW492c ↔ Academic Writing Skills_Kỹ năng viết học thuật</a>
-                </nav>
-            </div>
-            {isVisible && (
+          <select>
+            <option>ENW492C.40</option>
+          </select>
 
-                <div className="select-container">
-
-                    <select value={activityFilter} onChange={handleSelectChange} className="activity-select">
-                        <option>All Activities</option>
-                        <option>Hidden</option>
-                        <option>On Going</option>
-                        <option>Cancelled</option>
-                        <option>Completed</option>
-                        <option>Not Started</option>
-                        <option>Assignment or Feedback</option>
-                    </select>
-
-
-                    <select>
-                        <option>Slot 1</option>
-                        <option>Slot 2</option>
-                        <option>Slot 3</option>
-                        <option>Slot 4</option>
-                        <option>Slot 5</option>
-                        <option>Slot 6</option>
-                    </select>
-
-                    <select>
-                        <option>ENW492C.40</option>
-                    </select>
-
-                    <button onClick={() => navigateToExam()} className="button-exam">EXAM</button>
-                </div>
-            )}
-
-            <button onClick={toggleVisibility} style={{ color: 'blue', border: 'none', background: 'none', cursor: 'pointer' }}>
-                SHOW/HIDE (HIỆN/ẨN)
-            </button>
+          <button onClick={() => navigateToExam()} className="button-exam">
+            EXAM
+          </button>
         </div>
+      )}
 
+      <button
+        onClick={toggleVisibility}
+        style={{
+          color: "blue",
+          border: "none",
+          background: "none",
+          cursor: "pointer",
+        }}
+      >
+        SHOW/HIDE (HIỆN/ẨN)
+      </button>
+    </div>
+  );
+};
 
-
-    )
-}
-
-export default Header
+export default Header;
