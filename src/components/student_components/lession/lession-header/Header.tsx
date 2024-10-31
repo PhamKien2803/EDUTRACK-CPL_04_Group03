0fb0dd22d8a3af0467b476dd6dee2d6ab750e950
Nguyen Duc Long <longndhe171694@fpt.edu.vn>
2024-10-31 15:08:32 +0700

    update screen lession

--- src/components/student_components/lession/lession-header/Header.tsx
+++ src/components/student_components/lession/lession-header/Header.tsx
@@ -1,7 +1,49 @@
 import { useState } from 'react';
 import "../css/Lession.css";
 import { useNavigate } from 'react-router-dom';
-const Header: React.FC = () => {
+
+interface lession {
+    id: string,
+    SemesterID: string,
+    SlotID: string[]
+    CourseID: string,
+    StudentID: string,
+    LecturersID: string,
+    ClassID: string
+}
+
+interface participants {
+    id: string,
+    UserName: string,
+    Age: number,
+    Gender: true,
+    Address: string,
+    Email: string,
+    Password: string,
+    Image: string,
+    Role: number,
+    isOnline: boolean,
+    Status: boolean
+}
+
+interface classRoom {
+    ClassID: string,
+    ClassName: string,
+    Student: string[],
+    Status: boolean
+}
+
+
+
+
+interface Props {
+    lession: lession,
+    partcipants: participants[],
+    classes: classRoom[],
+    setselected: (id: string) => void
+}
+
+const Header: React.FC<Props> = ({ lession, partcipants, classes }) => {
     // State to track the selected option from the dropdown
     const [activityFilter, setActivityFilter] = useState('All Activities');
     const [isVisible, setIsVisible] = useState(true);
@@ -17,54 +59,60 @@ const Header: React.FC = () => {
     const navigateToExam = () => {
         navigate("/examtest");
     }
+    console.log(lession);
 
 
     return (
-        <div>
+        <>
 
-            <div className="course-container">
-                <nav className="breadcrumb">
-                    <a href="#">Home</a> / <a>ENW492c ↔ Academic Writing Skills_Kỹ năng viết học thuật</a>
-                </nav>
-            </div>
-            {isVisible && (
+            <div>
 
-                <div className="select-container">
-
-                    <select value={activityFilter} onChange={handleSelectChange} className="activity-select">
-                        <option>All Activities</option>
-                        <option>Hidden</option>
-                        <option>On Going</option>
-                        <option>Cancelled</option>
-                        <option>Completed</option>
-                        <option>Not Started</option>
-                        <option>Assignment or Feedback</option>
-                    </select>
-
-
-                    <select>
-                        <option>Slot 1</option>
-                        <option>Slot 2</option>
-                        <option>Slot 3</option>
-                        <option>Slot 4</option>
-                        <option>Slot 5</option>
-                        <option>Slot 6</option>
-                    </select>
-
-                    <select>
-                        <option>ENW492C.40</option>
-                    </select>
-
-                    <button onClick={() => navigateToExam()} className="button-exam">EXAM</button>
+                <div className="course-container">
+                    <nav className="breadcrumb">
+                        <a href="/homePage">Home</a> {lession?.CourseID} <a> ↔ Academic Writing Skills_Kỹ năng viết học thuật</a>
+                    </nav>
                 </div>
-            )}
+                {isVisible && (
 
-            <button onClick={toggleVisibility} style={{ color: 'blue', border: 'none', background: 'none', cursor: 'pointer' }}>
-                SHOW/HIDE (HIỆN/ẨN)
-            </button>
-        </div>
+                    <div className="select-container">
+
+                        <select value={activityFilter} onChange={handleSelectChange} className="activity-select">
+                            <option>All Activities</option>
+                            <option>Hidden</option>
+                            <option>On Going</option>
+                            <option>Cancelled</option>
+                            <option>Completed</option>
+                            <option>Not Started</option>
+                            <option>Assignment or Feedback</option>
+                        </select>
 
 
+                        <select>
+                            {lession.SlotID.map((id, index) => (
+                                <option key={`slot-${id}`}>slot {index + 1}</option>
+                            ))}
+
+
+                        </select>
+
+                        <select>
+                            <option>{classes.find(c => c.ClassID === lession?.ClassID)?.ClassName}</option>
+                        </select>
+
+                        <button onClick={() => navigateToExam()} className="button-exam">EXAM</button>
+                    </div>
+
+                )}
+
+                <button onClick={toggleVisibility} style={{ color: 'blue', border: 'none', background: 'none', cursor: 'pointer', marginLeft: '20px' }}>
+                    SHOW/HIDE (HIỆN/ẨN)
+                </button>
+                <div className='lecturer'>TEACHERS:  THOPN3@FPT.EDU.VN</div>
+            </div>
+
+
+
+        </>
 
     )
 }
--- src/service/ApiService.tsx
+++ src/service/ApiService.tsx
@@ -12,4 +12,45 @@ const getExamList = () => {
     return axios.get("Examination")
 }
 
-export { getDataExam, getAnswerForQuestionExam, getExamList } 
+const getSLot = () => {
+    return axios.get("Slot")
+}
+
+
+const getSLotById = (id: string) => {
+    return axios.get(`Slot/${id}`)
+}
+
+const getCourseById = (id: string) => {
+    return axios.get(`Course/${id}`)
+}
+
+const getCourseSemester = () => {
+    return axios.get("CourseSemester")
+}
+
+const getCourseSemesterById = (id: string) => {
+    return axios.get(`CourseSemester/${id}`)
+}
+
+const getParticipantsById = (id: string) => {
+    return axios.get(`Participants/${id}`)
+}
+
+const getParticipants = () => {
+    return axios.get("Participants")
+}
+
+const getQuestionSLot = () => {
+    return axios.get("QuestionSLot")
+}
+
+const getClass = () => {
+    return axios.get("Class")
+}
+
+export {
+    getDataExam, getAnswerForQuestionExam, getExamList, getSLot,
+    getCourseSemester, getCourseSemesterById, getParticipantsById, getParticipants,
+    getSLotById, getCourseById, getQuestionSLot, getClass
+} 
