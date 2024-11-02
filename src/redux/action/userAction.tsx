1cd84fbc412fe356156f806f65a13acb4ce2c3b3
TrungNghiaX <104517120+nnghiax@users.noreply.github.com>
2024-11-03 00:16:26 +0700

    add backend homepage

--- src/redux/action/userAction.tsx
+++ src/redux/action/userAction.tsx
@@ -1,20 +1,20 @@
-import { USER_LOGIN_SUCCESS } from "./types"
+import { USER_LOGIN_SUCCESS } from "./types";
 interface Participant {
-    UserID: string;
-    UserName: string;
-    Age: number;
-    Gender: boolean;
-    Address: string;
-    Email: string;
-    Password: string;
-    Image: string;
-    Role: number;
-    isOnline: boolean;
-    Status: "true" | "false";
+  id: string;
+  UserName: string;
+  Age: number;
+  Gender: boolean;
+  Address: string;
+  Email: string;
+  Password: string;
+  Image: string;
+  Role: number;
+  isOnline: boolean;
+  Status: "true" | "false";
 }
 export const doLogin = (data: Participant) => {
-    return {
-        type: USER_LOGIN_SUCCESS,
-        payload: data
-    }
-}
+  return {
+    type: USER_LOGIN_SUCCESS,
+    payload: data,
+  };
+};
