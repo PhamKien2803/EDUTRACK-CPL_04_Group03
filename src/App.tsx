8ce33f62b2fd0ccf2e95c4dcfc3877b1fb7664c5
PhuongHE173077 <phuongddhe173077@fpt.edu.vn>
2024-11-14 17:51:42 +0700

    update router authentication

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,51 @@
-import { Provider, useSelector } from 'react-redux';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { PersistGate } from 'redux-persist/integration/react';
+import { Route, Routes } from 'react-router-dom';
 import DashboardLayoutBranding from './layouts/dashboard-layouts/Dashboard';
 import LoginPage from './page/Auth/login/Login';
-import { persistor, store } from './redux/store';
-import { routesStudentHome } from './routes/routes';
+import { routesLecturersHome, routesStaffHome, routesStudentHome } from './routes/routes';
+import { Layout } from './routes/Layout';
+import { PrivateRoute } from './routes/PrivateRoute';
+import { useSelector } from 'react-redux';
 
 function App() {
-
-
-
+  const account = useSelector((state: any) => state.account.account);
 
   return (
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<LoginPage />} />
-            <Route path="/login" element={<LoginPage />} />
+    <Routes>
+      {/* Public routes */}
+      <Route path="/" element={<Layout />} />
+      <Route path="/login" element={<LoginPage />} />
+      {/* Private routes */}
+      <Route element={<PrivateRoute />}>
+        <Route element={<DashboardLayoutBranding />}>
+          {/* Student routes */}
+          {account.Role === 0 && routesStudentHome.map((route) => (
+            <Route
+              key={route.key}
+              path={route.path}
+              element={<route.component />}
+            />
+          ))}
 
+          {/* Lecturer routes */}
+          {account.Role === 1 && routesLecturersHome.map((route) => (
+            <Route
+              key={route.key}
+              path={route.path}
+              element={<route.component />}
+            />
+          ))}
 
-            <Route element={<DashboardLayoutBranding />}>
-              {routesStudentHome.map((route) => (
-                <Route
-                  key={route.key}
-                  path={route.path}
-                  element={<route.component />}
-                />
-              ))}
-            </Route>
-          </Routes>
-        </BrowserRouter>
-      </PersistGate>
-    </Provider>
+          {/* Staff routes */}
+          {account.Role === 2 && routesStaffHome.map((route) => (
+            <Route
+              key={route.key}
+              path={route.path}
+              element={<route.component />}
+            />
+          ))}
+        </Route>
+      </Route>
+    </Routes>
   );
 }
 
--- src/routes/PrivateRoute.tsx
+++ src/routes/PrivateRoute.tsx
@@ -1,16 +1,12 @@
 import React from 'react'
 import { useSelector } from 'react-redux';
-import { Navigate } from 'react-router-dom';
+import { Navigate, Outlet } from 'react-router-dom';
 
-export const PrivateRoute = ({ children }) => {
+export const PrivateRoute = () => {
     const isAuthenticated = useSelector((state: any) => state.account.isAuthenticated);
 
-    if (!isAuthenticated) {
-        return <Navigate to={'/login'}></Navigate>
-    }
+
     return (
-        <>
-            {children}
-        </>
+        isAuthenticated ? <Outlet /> : <Navigate to={'/login'} replace />
     )
 }
