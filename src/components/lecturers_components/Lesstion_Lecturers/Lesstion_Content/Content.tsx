507b533b569557b86b981dce3e35431607877d76
Nguyen Duc Long <longndhe171694@fpt.edu.vn>
2024-11-14 20:08:22 +0700

    screen lession lecturers

--- src/components/lecturers_components/Lesstion_Lecturers/Lesstion_Content/Content.tsx
+++ src/components/lecturers_components/Lesstion_Lecturers/Lesstion_Content/Content.tsx
@@ -15,10 +15,10 @@ interface Props {
     participants: participants[];
     classes: classRoom[];
     courses: courses[];
-    setclassId:(id:string)=>void
+    setclassId: (id: string) => void
 }
 
-const Content: React.FC<Props> = ({ questionSlot, slot, lession, participants, classes, courses,setclassId }) => {
+const Content: React.FC<Props> = ({ questionSlot, slot, lession, participants, classes, courses, setclassId }) => {
     const [visibleSlots, setVisibleSlots] = useState<{ [key: string]: boolean }>({});
 
     const toggleVisibility = (slotId: string) => {
@@ -29,101 +29,99 @@ const Content: React.FC<Props> = ({ questionSlot, slot, lession, participants, c
     };
 
     console.log(classes);
-    
+
 
     return (
         <div>
             <main>
                 <Grid container spacing={3}>
-                    {lession.SlotID.map((item, index) => (
-                        <Grid item xs={12} md={8} key={`slot-grid-${index}`}>
-                            <Box
-                                mb={2}
-                                p={2}
-                                border={1}
-                                borderRadius={2}
-                                borderColor="grey.300"
-                                boxShadow={2}
-                                onClick={() => toggleVisibility(item)}
-                            >
-                                <Box bgcolor="grey.100" borderRadius="8px" p={2}>
-                                    <Box display="flex" justifyContent="space-between" alignItems="center">
-                                        <Typography variant="h6" component="div" bgcolor="lightpink" borderRadius={2} p={0.5}>
-                                            Slot{index +1}: 
-                                        </Typography>
-                                        
-                                        <Box display="flex" alignItems="center" gap={2}>
-                                            <Typography variant="body2" color="textSecondary">
-                                                Timestart-timeEnd
+                    <Grid item xs={12} md={8}>
+                        <Grid container spacing={3}>
+                            {lession.SlotID.map((item, index) => (
+                                <Grid item xs={12} key={`slot-grid-${index}`}>
+                                    <Box
+                                        mb={2}
+                                        p={2}
+                                        border={1}
+                                        borderRadius={2}
+                                        borderColor="grey.300"
+                                        boxShadow={2}
+                                        onClick={() => toggleVisibility(item)}
+                                    >
+                                        <Box bgcolor="grey.100" borderRadius="8px" p={2}>
+                                            <Box display="flex" justifyContent="space-between" alignItems="center">
+                                                <Typography variant="h6" component="div" bgcolor="lightpink" borderRadius={2} p={0.5}>
+                                                    Slot {index + 1}:
+                                                </Typography>
+                                                <Box display="flex" alignItems="center" gap={2}>
+                                                    <Typography variant="body2" color="textSecondary">
+                                                        Timestart-timeEnd
+                                                    </Typography>
+                                                    <Link href={`/lecturer/session-details?Slotid=${item}&classid=${classes[0]?.ClassID}`}>
+                                                        <Button component={Link} variant="outlined" color="secondary" size="small">
+                                                            Update Slot
+                                                        </Button>
+                                                    </Link>
+                                                </Box>
+                                            </Box>
+                                            <Typography variant="body1" color="textSecondary" mt={1} mb={1} fontWeight="bold">
+                                                {slot.find(sl => sl.id === item)?.Description.split('\n').map((line, index) => (
+                                                    <React.Fragment key={index}>
+                                                        {line}
+                                                        <br />
+                                                    </React.Fragment>
+                                                ))}
                                             </Typography>
-                                            <Link href={`/lecturer/session-details?Slotid=${item}&classid=${classes[0]?.ClassID}`}>
-                                            <Button component={Link} variant="outlined" color="secondary" size="small">
-                                                Update Slot
-                                            </Button></Link>
                                         </Box>
+
+                                        <Divider sx={{ my: 2 }} />
+
+                                        <Collapse in={visibleSlots[item]}>
+                                            <Typography variant="subtitle1" color="primary" gutterBottom>
+                                                Questions:
+                                            </Typography>
+                                            <List>
+                                                {questionSlot
+                                                    .filter((qs) => qs.Slotid === item)
+                                                    .map((qs, qIndex) => (
+                                                        <ListItem
+                                                            key={`qs-${qIndex}`}
+                                                            sx={{
+                                                                display: 'flex',
+                                                                alignItems: 'center',
+                                                                cursor: 'pointer',
+                                                                '&:hover': {
+                                                                    backgroundColor: 'rgba(0, 123, 255, 0.1)',
+                                                                    transform: 'scale(1.02)',
+                                                                },
+                                                                transition: 'background-color 0.3s, transform 0.3s',
+                                                            }}
+                                                        >
+                                                            <ListItemIcon>
+                                                                <HelpOutlineIcon color="action" />
+                                                            </ListItemIcon>
+                                                            <ListItemText
+                                                                primary={`Q${qIndex + 1}: ${qs.content.substring(0, 50)}...`}
+                                                                secondary={qs.Status === 0 ? 'Not started' : 'On-Going'}
+                                                                secondaryTypographyProps={{
+                                                                    color: qs.Status === 0 ? 'error' : 'success',
+                                                                    fontWeight: 'bold',
+                                                                }}
+                                                            />
+                                                            <ArrowForwardIosIcon fontSize="small" color="action" />
+                                                        </ListItem>
+                                                    ))}
+                                            </List>
+                                        </Collapse>
                                     </Box>
-                                    <Typography variant="body1" color="textSecondary" mt={1} mb={1} fontWeight="bold">
-                                    {slot.find(sl=>sl.id===item)?.Description}
-                                    </Typography>
-                                </Box>
-
-                                <Divider sx={{ my: 2 }} />
-
-                                <Collapse in={visibleSlots[item]}>
-                                    <Typography variant="subtitle1" color="primary" gutterBottom>
-                                        Questions:
-                                    </Typography>
-                                    <List>
-                                        {questionSlot
-                                            .filter((qs) => qs.Slotid === item)
-                                            .map((qs, qIndex) => (
-                                                <ListItem
-                                                    key={`qs-${qIndex}`}
-                                                    sx={{
-                                                        display: 'flex',
-                                                        alignItems: 'center',
-                                                        cursor: 'pointer',
-                                                        '&:hover': {
-                                                            backgroundColor: 'rgba(0, 123, 255, 0.1)',
-                                                            transform: 'scale(1.02)',
-                                                        },
-                                                        transition: 'background-color 0.3s, transform 0.3s',
-                                                    }}
-                                                >
-                                                    <ListItemIcon>
-                                                        <HelpOutlineIcon color="action" />
-                                                    </ListItemIcon>
-                                                    <ListItemText
-                                                        primary={`Q${qIndex + 1}: ${qs.content.substring(0, 50)}...`}
-                                                        secondary={qs.Status === 0 ? 'Not started' : 'On-Going'}
-                                                        secondaryTypographyProps={{
-                                                            color: qs.Status === 0 ? 'error' : 'success',
-                                                            fontWeight: 'bold',
-                                                        }}
-                                                    />
-                                                    <ArrowForwardIosIcon fontSize="small" color="action" />
-                                                </ListItem>
-                                            ))}
-                                    </List>
-                                </Collapse>
-                            </Box>
+                                </Grid>
+                            ))}
                         </Grid>
-                    ))}
+                    </Grid>
 
                     <Grid item xs={12} md={4}>
                         <Card variant="outlined" style={{ padding: '20px' }}>
                             <Typography variant="h6">Class</Typography>
-                            {/* <Select variant="outlined" sx={{ minWidth: 200 }} onChange={e=>setclassId(e.target.value)} size="small" displayEmpty defaultValue={lession.ClassID}>
-                                {
-                                    classes.map((cls,index)=>(
-                                        <MenuItem key={index} value={cls.ClassID}>
-                                            {cls.ClassName}
-                                        </MenuItem>
-                                    ))
-                                }
-                                
-                                
-                            </Select> */}
                             <Divider style={{ margin: '10px 0' }} />
                             <Typography variant="body2">0 online</Typography>
                             <Typography variant="body2">20 students</Typography>
@@ -135,7 +133,7 @@ const Content: React.FC<Props> = ({ questionSlot, slot, lession, participants, c
                                 <Typography variant="body1">Lecturer (2)</Typography>
                                 <List>
                                     <ListItem>edu_next_ltr_fpt_edu_01</ListItem>
-                                    <ListItem>{participants.find(pr=>pr.id === lession.LecturersID)?.Email}</ListItem>
+                                    <ListItem>{participants.find(pr => pr.id === lession.LecturersID)?.Email}</ListItem>
                                 </List>
                             </div>
 
@@ -146,6 +144,7 @@ const Content: React.FC<Props> = ({ questionSlot, slot, lession, participants, c
                     </Grid>
                 </Grid>
             </main>
+
         </div>
     );
 };
