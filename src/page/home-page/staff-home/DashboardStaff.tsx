355746ced4669859badc53e3128ecb96e422fc6e
PhamKien <kienpdhe170155@fpt.edu.vn>
2024-11-21 23:29:03 +0700

    Fix code UI Admin

--- src/page/home-page/staff-home/DashboardStaff.tsx
+++ src/page/home-page/staff-home/DashboardStaff.tsx
@@ -1,32 +1,10 @@
-import { Box, Stack } from "@mui/material"
-import Header from "../../../components/staff_components/ComponentsPage/layouts/Header"
-import MainGrid from "../../../components/staff_components/ComponentsPage/layouts/MainGrid"
-import { alpha } from '@mui/material/styles';
 
 function DashboardStaff() {
-    return (
-        <Box
-            component="main"
-            sx={(theme) => ({
-                flexGrow: 1,
-                backgroundColor: alpha(theme.palette.background.default, 1),
-                overflow: 'auto',
-            })}
-        >
-            <Stack
-                spacing={2}
-                sx={{
-                    alignItems: 'center',
-                    mx: 3,
-                    pb: 5,
-                    mt: { xs: 8, md: 0 },
-                }}
-            >
-                <Header />
-                <MainGrid />
-            </Stack>
-        </Box>
-    )
+  return (
+    <div>
+      
+    </div>
+  )
 }
 
 export default DashboardStaff
