c37f53d1a8196591a2e236d6fb9e006d11732bc3
PhamKien <kienpdhe170155@fpt.edu.vn>
2024-11-26 12:42:30 +0700

    Update UI Exam

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -161,12 +161,7 @@ const routesLecturersHome: Router[] = [
     path: "/lecturer/lession-course",
     component: Lession_Lecturers,
   },
-  {
-    name: "Profile",
-    key: "ProfilePage",
-    path: "/profile",
-    component: Profile,
-  },
+  
   {
     name: "lession",
     key: "LessionPage",
